0ac2d144b5d9202a21a0540b39c929795ca354e8
honeymaro <honeymaro@live.co.kr>
2019-01-15 18:39:51 +0900

    Static 디렉토리를 다중 지정할 수 있도록 하고 버그 수정한다
    - Static 디렉토리 입력을 배열로 받을 수 있게 함
    - 루트 위치를 정확히 함

--- pages.js
+++ pages.js
@@ -6,11 +6,46 @@ var app = {};
 var options = {};
 
 
+
+
+var changePathToAbsolute = function (filePath) {
+    if (!path.isAbsolute(filePath)) {
+        filePath = path.join(path.resolve("./"), filePath);
+    }
+    return filePath;
+}
+
+var ensureDirectoryExistence = function (filePath) {
+    var dirname = path.dirname(filePath);
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    ensureDirectoryExistence(dirname);
+    fs.mkdirSync(dirname);
+}
+
+var copyFolderSync = function (from, to) {
+    if (!fs.existsSync(to)) {
+        fs.mkdirSync(to);
+    }
+    from = changePathToAbsolute(from);
+    to = changePathToAbsolute(to);
+    fs.readdirSync(from).forEach(element => {
+        if (fs.lstatSync(path.join(from, element)).isFile()) {
+            fs.copyFileSync(path.join(from, element), path.join(to, element));
+        } else {
+            copyFolderSync(path.join(from, element), path.join(to, element));
+        }
+    });
+}
+
 var _mergeOptions = function (_options) {
     var options = {
-        path: path.join(process.env.PWD, "docs")
+        path: path.join(path.resolve("./"), "docs"),
+        watch: false
     };
     options = Object.assign({}, options, _options);
+    options.path = changePathToAbsolute(options.path);
     return options;
 
 }
@@ -38,35 +73,22 @@ var _renderFiles = function (list = []) {
 
 
 
-var ensureDirectoryExistence = function (filePath) {
-    var dirname = path.dirname(filePath);
-    if (fs.existsSync(dirname)) {
-        return true;
-    }
-    ensureDirectoryExistence(dirname);
-    fs.mkdirSync(dirname);
-}
-
-var copyFolderSync = function (from, to) {
-    if (!fs.existsSync(to)) {
-        fs.mkdirSync(to);
-    }
-    fs.readdirSync(from).forEach(element => {
-        if (fs.lstatSync(path.join(from, element)).isFile()) {
-            fs.copyFileSync(path.join(from, element), path.join(to, element));
-        } else {
-            copyFolderSync(path.join(from, element), path.join(to, element));
-        }
-    });
-}
-
 
 module.exports = (function (_app, _options) {
     app = _app;
     options = _mergeOptions(_options);
-
+    console.log(require.main.filename);
+    console.log(process.mainModule.filename);
+    console.log(path.resolve("./"));
     if (options.static) {
-        copyFolderSync(options.static, options.path);
+        if (!Array.isArray(options.static)) {
+            options.static = [options.static];
+        }
+        var i = 0;
+        for (i = 0; i < options.static.length; i++) {
+            console.log(options.static[i], options.path);
+            copyFolderSync(options.static[i], options.path);
+        }
     }
 
     return {
